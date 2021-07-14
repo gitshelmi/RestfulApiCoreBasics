b35cd35867f0dbdf14c89d6c9ec9bfdfaed62b4f
Shahab <shahab.helmi@ucdenver.edu>
2021-07-14 15:57:04 -0600

    Add project files.

---
+++ RACB.API/Controllers/AuthorsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using RACB.API.Services;
+
+namespace RACB.API.Controllers
+{
+    [ApiController]
+    [Route("api/authors")]
+    public class AuthorsController : ControllerBase
+    {
+        private readonly ICourseRepository _courseRepository;
+
+        public AuthorsController(ICourseRepository courseRepository)
+        {
+            _courseRepository = courseRepository ??
+                                throw new ArgumentException(nameof(courseRepository));
+        }
+
+        [HttpGet]
+        public IActionResult GetAuthors()
+        {
+            var authors = _courseRepository.GetAuthors();
+            return Ok(authors);
+        }
+
+        [HttpGet("{authorId}")]
+        public IActionResult GetAuthor(Guid authorId)
+        {
+            var author = _courseRepository.GetAuthor(authorId);
+
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(author);
+        }
+    }
+}
---
+++ RACB.API/Services/ICourseRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using RACB.API.Models;
+
+namespace RACB.API.Services
+{
+    public interface ICourseRepository
+    {    
+        IEnumerable<Course> GetCourses(Guid authorId);
+        Course GetCourse(Guid authorId, Guid courseId);
+        void AddCourse(Guid authorId, Course course);
+        void UpdateCourse(Course course);
+        void DeleteCourse(Course course);
+        IEnumerable<Author> GetAuthors();
+        Author GetAuthor(Guid authorId);
+        IEnumerable<Author> GetAuthors(IEnumerable<Guid> authorIds);
+        void AddAuthor(Author author);
+        void DeleteAuthor(Author author);
+        void UpdateAuthor(Author author);
+        bool AuthorExists(Guid authorId);
+        bool Save();
+    }
+}
